db52758148a11887268b1c375e1244ac0a84c51f
Sy Nguyen Nguyen <160448378+synguyen446@users.noreply.github.com>
2024-08-29 22:27:59 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,3 @@ from user_interface import *
 flight_search_app = FlightSearchApp()
 
 flight_search_app.run()
-
-#add the function for one way or roundtrip
-#raise exception when the required fields are empty
-#format output
-#insert label when entry is empty
